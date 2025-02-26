b0e598a6180c3e0f7effadb3344a79a991a619c2
Lautaro Reche <lautaroreche1@gmail.com>
2025-02-26 08:22:17 +0100

    Add filters to admin panel

--- ecommerce_app/admin.py
+++ ecommerce_app/admin.py
@@ -3,21 +3,21 @@ from ecommerce_app.models import Product, User, Order, Payment, SuscriptorNewsle
 from django.contrib.sessions.models import Session
 
 
-admin.site.register(Session)
-
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'description', 'image', 'category', 'stock')
+    list_filter = ('category',)
     search_fields = ('id', 'name', 'price', 'description', 'image', 'category', 'stock')
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'email', 'password', 'phone', 'address', 'city', 'state', 'zip_code')
+    list_filter = ('city', 'state', 'zip_code',)
     search_fields = ('id', 'first_name', 'last_name', 'email', 'password', 'phone', 'address', 'city', 'state', 'zip_code')
 
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'get_products', 'total', 'address', 'date', 'status', 'payment')
+    list_filter = ('status',)
     search_fields = ('id', 'user', 'products', 'total', 'address', 'date', 'status', 'payment')
     def get_products(self, obj):
         return ", ".join([p.name for p in obj.products.all()])
@@ -26,6 +26,7 @@ class OrderAdmin(admin.ModelAdmin):
 
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'order', 'method', 'detail', 'amount', 'date', 'status', 'order')
+    list_filter = ('method', 'status',)
     search_fields = ('id', 'user', 'order', 'method', 'detail', 'amount', 'date', 'status', 'order')
 
 
@@ -34,6 +35,7 @@ class SuscriptorNewsletterAdmin(admin.ModelAdmin):
     search_fields = ('id', 'email')
 
 
+admin.site.register(Session)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(User, UserAdmin)
 admin.site.register(Order, OrderAdmin)
