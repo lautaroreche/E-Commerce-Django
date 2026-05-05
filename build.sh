f7504fab82e7d67a12942ca8afe3f4586eecabae
Lautaro Reche <lautaroreche1@gmail.com>
2026-05-05 08:20:06 +0100

    fix: add --clear to collectstatic to force recopy on each deploy

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 npm install
 npm run build:css
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 python manage.py migrate
